1207b2b2385750069ead9e250a21939af7020e83
Niklas Schmelzle <niklas.schmelzle@gmail.com>
2022-12-12 14:54:07 -0500

    added cooling to cpu implementation of new data structure

--- src/algorithms/path_sgd_layout.cpp
+++ src/algorithms/path_sgd_layout.cpp
@@ -466,6 +466,11 @@ namespace odgi {
             config.eta_max = eta_max;
             config.eps = eps;
             config.iter_with_max_learning_rate = (int32_t)  iter_with_max_learning_rate;
+            config.first_cooling_iteration = std::floor(cooling_start * (double)iter_max);
+            config.theta = theta;
+            config.space = space;
+            config.space_max = space_max;
+            config.space_quantization_step = space_quantization_step;
             config.nthreads = nthreads;
 
             cuda::cuda_layout(config, dynamic_cast<const odgi::graph_t&>(graph), X, Y);
