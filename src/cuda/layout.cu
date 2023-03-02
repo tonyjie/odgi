7670dc6822bd8e3593c6f06c773ffa6eff363a80
Niklas Schmelzle <niklas.schmelzle@gmail.com>
2023-03-02 14:07:53 -0500

    path selection split in multiple section
    but currently slower

--- src/cuda/layout.cu
+++ src/cuda/layout.cu
@@ -49,17 +49,49 @@ __global__ void cuda_device_layout(int iter, cuda::layout_config_t config, curan
 
     // TODO improve discrete distribution selection
     // select path
-    int32_t offset = (int32_t)(ceil((curand_uniform(rnd_state) * (gridDim.x + 1.0))) - 1.0);
-    uint32_t step_idx = ((uint32_t) (offset * blockDim.x + threadIdx.x)) % (uint32_t) path_data.total_path_steps;
-    uint32_t path_idx;  // = blockIdx.x % path_data.path_count;
-    uint32_t start_step_path = 0;
-    for (int pidx = 0; pidx < path_data.path_count; pidx++) {
-        if (step_idx >= start_step_path && step_idx < start_step_path + path_data.paths[pidx].step_count) {
-            path_idx = pidx;
-            break;
-        } else {
-            start_step_path += path_data.paths[pidx].step_count;
+    // generate first step
+    __shared__ uint32_t first_step_idx;
+    __shared__ uint32_t first_step_path_idx;
+    __shared__ bool first_step_path_idx_found;
+    // TODO use less shared memory
+    if (threadIdx.x == 0) {
+        first_step_idx = (uint32_t)(ceil(curand_uniform(rnd_state + threadIdx.x) * (path_data.total_path_steps + 1.0)));
+        first_step_path_idx_found = false;
+        assert(first_step_idx < path_data.total_path_steps);
+    }
+    __syncthreads();
+    // find path of first step
+    // TODO parallelize
+    if (threadIdx.x % 32 == 0 && !first_step_path_idx_found) {
+        for (int pidx = 0; pidx < path_data.path_count; pidx++) {
+            if (first_step_idx >= path_data.paths[pidx].first_step_in_path && first_step_idx < path_data.paths[pidx].first_step_in_path + path_data.paths[pidx].step_count) {
+                first_step_path_idx = pidx;
+                first_step_path_idx_found = true;
+                break;
+            }
         }
+    }
+    __shared__ bool cooling;
+    if (tid % 32 == 0) {
+        cooling = (iter >= config.first_cooling_iteration) || (curand_uniform(rnd_state + threadIdx.x) <= 0.5);
+    }
+    __syncwarp();
+    assert(first_step_path_idx_found == true);
+    // find path of step of specific thread
+    uint32_t step_idx = (first_step_idx + threadIdx.x) % path_data.total_path_steps;
+    uint32_t path_idx;
+    bool path_idx_found = false;
+    for (int i = 0; i < path_data.path_count; i++) {
+        int pidx = (i + first_step_path_idx) % path_data.path_count;
+        if (step_idx >= path_data.paths[pidx].first_step_in_path && step_idx < path_data.paths[pidx].first_step_in_path + path_data.paths[pidx].step_count) {
+            path_idx = pidx;
+            path_idx_found = true;
+            break;
+        }
+    }
+    assert(path_idx_found == true);
+    if (threadIdx.x % 32 == 31) {
+        first_step_path_idx = path_idx;
     }
 
     path_t p = path_data.paths[path_idx];
@@ -70,13 +102,6 @@ __global__ void cuda_device_layout(int iter, cuda::layout_config_t config, curan
 
     uint32_t s1_idx = (uint32_t)(ceil((curand_uniform(rnd_state + threadIdx.x)*float(p.step_count))) - 1.0);
     uint32_t s2_idx;
-
-
-    __shared__ bool cooling;
-    if (tid % 32 == 0) {
-        cooling = (iter >= config.first_cooling_iteration) || (curand_uniform(rnd_state + threadIdx.x) <= 0.5);
-    }
-    __syncwarp();
 
     if (cooling) {
         bool backward;
@@ -528,6 +553,7 @@ void cuda_layout(layout_config_t config, const odgi::graph_t &graph, std::vector
 
         int step_count = graph.get_step_count(p);
         path_data.paths[path_idx].step_count = step_count;
+        path_data.paths[path_idx].first_step_in_path = 0;
 
         cuda::path_element_t *cur_path;
         cudaMallocManaged(&cur_path, step_count * sizeof(path_element_t));
@@ -557,6 +583,12 @@ void cuda_layout(layout_config_t config, const odgi::graph_t &graph, std::vector
                 std::cout << "Error: Here should be another step" << std::endl;
             }
         }
+    }
+    // precompute rank of first step of each path to ease later parallelisation
+    uint32_t step_counter = 0;
+    for (int path_idx = 0; path_idx < path_count; path_idx++) {
+        path_data.paths[path_idx].first_step_in_path = step_counter;
+        step_counter += path_data.paths[path_idx].step_count;
     }
 
 
