6042748fc49af72641ad2c3bffbebfff80f5f1f9
tonyjie <jiajieli0429@gmail.com>
2024-01-13 15:33:32 -0500

    merge branch[metrics] function into this branch. Now we can use [odgi tension] in this branch

--- src/algorithms/layout.hpp
+++ src/algorithms/layout.hpp
@@ -20,6 +20,8 @@ void to_tsv(std::ostream &out,
 
 union conv_t { uint64_t i; double d; };
 
+double coord_dist(const xy_d_t, const xy_d_t);
+
 class Layout {
     sdsl::enc_vector<> xy;
     double min_value = std::numeric_limits<double>::max();
